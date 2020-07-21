052db4d7278276bfa0ec3ceb9e8ec3c525e85db7
Riccardo <riccardo.zuliani99@gmail.com>
2020-07-21 09:50:41 +0100

    Lezione 4, Database con comandi

--- blog/flaskblog.py
+++ blog/flaskblog.py
@@ -1,8 +1,38 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect #import necessari per il funzionamento dell'applicazione
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'    #chiave necessaria per l'applicazione
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) # va ad indicare che c'è una relazione con Post con riferimento ad author e che il caricamento deve essere effettuato in maniera immediata
+    # non è una colonna effettiva della tabella
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # questa è una colonna effettiva e va ad indicare la presenza di una chiave esterna che punta a user.id
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [       #i post che visualizzaremo all'interno
     {
